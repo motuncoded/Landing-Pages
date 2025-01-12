b87815a7858fbca28019bf5ba37d7712cf9e79f3
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2025-01-12 12:01:27 +0100

    landing page 8

---
+++ 7/script.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      backgroundImage: {
+        "multi-gradient":
+          "linear-gradient(to bottom right, #f97316 , #ec4899 , #a855f7 , #3b82f6 , #000043)", // Add your gradient here
+      },
+    },
+  },
+};
